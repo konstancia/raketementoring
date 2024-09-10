2b219056c0e51d1b9e159863f909d1966044320b
Konstancia Milova <konstanciamilova@Konstancias-Laptop.local>
2024-09-10 18:21:32 -0400

    first commit

---
+++ ConsoleApp2/CostCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace CostCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int numberOne = 15;
+            int numberTwo = 20;
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            
+
+            int result = numberOne * numberTwo;
+            Console.WriteLine(result);
+
+            string myText = Console.ReadLine();
+            
+            Console.WriteLine("You entered: " + myText);
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Fred!");
+        }
+
+    }
+}
