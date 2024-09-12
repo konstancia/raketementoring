7aa5e8f67d1a6522e5d2d951016d241bd52a70b2
Konstancia Milova <konstanciamilova@Konstancias-Laptop.local>
2024-09-11 20:54:28 -0400

    practice

---
+++ ConsoleApp2/Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string aString = "Hello";
+            string pamela = "what";
+            string tanga = "oh!";
+            int aNumber = 5;
+            
+            Console.WriteLine(aString + "this is me" + pamela + "is your name" + tanga);
+            
+            //int numberOne = 15;
+            int numberTwo = 20;
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            
+
+            int result = numberOne * numberTwo;
+            Console.WriteLine(result);
+
+            string myText = Console.ReadLine();
+            
+            Console.WriteLine("You entered: " + myText);
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Fred!");
+        }
+
+    }
+}
