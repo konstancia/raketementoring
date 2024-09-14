4d2c6642c538c0038344703f474ea2d6999714bf
Blueyeskon <konstancia_milova@hotmail.com>
2024-09-14 10:54:15 -0400

    costcalculator

---
+++ ConsoleApp2/CostCalculator/CostCalculatorFinal.cs
@@ -0,0 +1,33 @@
+// See https://aka.ms/new-console-template for more information
+
+namespace CostCalculatorFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter width of flooring.");
+            string floorWidth = Console.ReadLine();
+            
+            Console.WriteLine("Please enter length flooring.");
+            string floorLength = Console.ReadLine();
+            
+            Console.WriteLine("Please enter cost per 1 unit of flooring.");
+            string floorCost = Console.ReadLine();
+            
+            //int floorWidth = 20;
+            //int floorLength = 15;
+            //int floorCost = 50;
+            //int area = floorWidth * floorLength ;
+          
+            //string floorW = Console.ReadLine();
+            
+        
+        
+            //int totalCost = (area);
+            //Console.WriteLine(totalCost);
+        }
+
+    }
+}
