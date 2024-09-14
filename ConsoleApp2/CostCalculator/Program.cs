a0c8e3cfa8452551453a085b8195f833f2136b07
Blueyeskon <konstancia_milova@hotmail.com>
2024-09-14 10:52:39 -0400

    changes

--- ConsoleApp2/CostCalculator/Program.cs
+++ ConsoleApp2/CostCalculator/Program.cs
@@ -6,15 +6,27 @@ namespace CostCalculator
     {
         static void Main(string[] args)
         {
-            int floorWidth = 20;
-            int floorLength = 15;
-            int floorCost = 50;
-            int area = floorWidth + floorLength * floorCost;
             
-        Console.WriteLine("Please enter width, length, and the cost per 1 unit of flooring.");
+            Console.WriteLine("Please enter width of flooring.");
+            string floorWidth = Console.ReadLine();
+            
+            Console.WriteLine("Please enter length flooring.");
+            string floorLength = Console.ReadLine();
+            
+            Console.WriteLine("Please enter cost per 1 unit of flooring.");
+            string floorCost = Console.ReadLine();
+            
+            //int floorWidth = 20;
+            //int floorLength = 15;
+            //int floorCost = 50;
+            //int area = floorWidth * floorLength ;
+          
+            //string floorW = Console.ReadLine();
+            
         
-        int totalCost = (area);
-        Console.WriteLine(totalCost);
+        
+        //int totalCost = (area);
+        //Console.WriteLine(totalCost);
     }
 
 }
--- ConsoleApp2/Practice/obj/Debug/net8.0/Practice.AssemblyInfo.cs
+++ ConsoleApp2/Practice/obj/Debug/net8.0/Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d91f5b71a6bb5b83acbc1dbfe57527fed92d7db4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf28a967c6926c24bd75e394292e2b3f7c2889ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
