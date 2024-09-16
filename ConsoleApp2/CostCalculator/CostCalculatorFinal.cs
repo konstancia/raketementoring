5a7c805879d554f42d35c8862126dd2793e978b6
Blueyeskon <konstancia_milova@hotmail.com>
2024-09-16 15:42:23 -0400

    cost calculator adding convert

--- ConsoleApp2/CostCalculator/CostCalculatorFinal.cs
+++ ConsoleApp2/CostCalculator/CostCalculatorFinal.cs
@@ -6,24 +6,28 @@ namespace CostCalculatorFinal
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello, user. You will be prompted to enter width, length, and the cost per 1 unit of flooring");
+
+            Console.WriteLine("Please enter width");
+            string myWidth = Convert.ToInt32(Console.ReadLine);
+
+            Console.WriteLine ("you entered width:" + myWidth);
+
+            Console.WriteLine ("Please enter length");
+            string myLength = Convert.ToInt32(Console.ReadLine);
+
+            Console.WriteLine ("You entered Lengh" + myLength);
+
+
+            int area = myWidth + myLength;
+            Console.WriteLine("Total area for project" + area);
             
-            Console.WriteLine("Please enter width of flooring.");
-            int floorWidth = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Please enter length flooring.");
-            int floorLength = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Please enter cost per 1 unit of flooring.");
-            int floorCost = int.Parse(Console.ReadLine());
-            
-            //int floorWidth = 20;
-            //int floorLength = 15;
-            //int floorCost = 50;
-            int area = floorWidth * floorLength ;
-        
+
             int totalCost = (area);
             Console.WriteLine(totalCost);
         }
 
     }
+
 }
