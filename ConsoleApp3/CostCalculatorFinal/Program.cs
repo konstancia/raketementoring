05c89b6b47b4b5b1eccddf407d4ad1aeb350d4fb
Blueyeskon <konstancia_milova@hotmail.com>
2024-09-23 21:16:28 -0400

    if/else statement

--- ConsoleApp3/CostCalculatorFinal/Program.cs
+++ ConsoleApp3/CostCalculatorFinal/Program.cs
@@ -6,9 +6,14 @@ namespace CostCalculatorFinal
     {
         static void Main(string[] args)
         {
+
+            const string rectangle = "square";
+            const string circle = "circle";
+
+
             Console.WriteLine("Please choose another shape for non-rectangular room: ex: (circle, triangle");
             string Shape = (Console.ReadLine());
-            
+
             Console.WriteLine("You choose the following shape: " + Shape);
 
             Console.WriteLine(
@@ -22,44 +27,59 @@ namespace CostCalculatorFinal
             Console.WriteLine("Please enter length: ");
             int Length = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("You entered Lengh: " + Length);
-            
-            int area = Width + Length;
-            Console.WriteLine("Total area for project: " + area);
-            
+            Console.WriteLine("You entered Length: " + Length);
+
+            int areaSquare = Width + Length;
+            Console.WriteLine("Total area for project: " + areaSquare);
+
             Console.WriteLine("Now we're going to calculate the cost for your project");
 
-            double cost = 5;
-                
-            double totalCost = (cost * area);
+            int costPerHourSquare = 86;
+
+            int totalCost = (costPerHourSquare * areaSquare);
             Console.WriteLine("Your total project cost: " + totalCost);
-            
+
+            if (totalCost < 500)
+            {
+                Console.WriteLine(" Good Deal");
+            }
+            else
+            {
+                Console.WriteLine(" Hope you have a coupon");
+            }
+
+
             Console.WriteLine("Please choose another shape for non-rectangular room: ex: (circle, triangle");
             string roomShape = (Console.ReadLine());
-            
+
             Console.WriteLine("You choose the following shape: " + roomShape);
-            
+
             Console.WriteLine("Please enter height of the circle: ");
             int circleLenght = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine("Please enter widgth of the circle: ");
+
+            Console.WriteLine("Please enter width of the circle: ");
             int circleWidth = Convert.ToInt32(Console.ReadLine());
-            
+
             int areaCircle = circleLenght * circleWidth;
-            
+
             Console.WriteLine("Total area for project: " + areaCircle);
-            
+
             Console.WriteLine("Now we're going to calculate the cost for your new project");
 
-            double costCircle = 8;
-                
-            double totalCostCircle = (cost * area);
+            int  costPerHourCircle = 86;
+
+            double totalCostCircle = (costPerHourCircle * areaCircle);
             Console.WriteLine("Your total project cost: " + totalCostCircle);
             
-            
-
-
-        }
+            if (totalCostCircle < 10000)
+            {
+                Console.WriteLine(" Good Deal");
+            }
+            else
+            {
+                Console.WriteLine(" Hope you have a coupon");
+            }
+    }
 
     }
     
--- ConsoleApp3/CostCalculatorFinal/obj/Debug/net8.0/CostCalculatorFinal.AssemblyInfo.cs
+++ ConsoleApp3/CostCalculatorFinal/obj/Debug/net8.0/CostCalculatorFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CostCalculatorFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06683214374cf2f1410c2fb2282ff265f664b195")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1dc35a3e36e9c8ed19a788884beed077347e857")]
 [assembly: System.Reflection.AssemblyProductAttribute("CostCalculatorFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CostCalculatorFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
